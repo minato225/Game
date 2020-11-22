ba458587ec89701102a43aaf38a568ce29afb775
Roman_Doskoch <49292434+minato225@users.noreply.github.com>
2020-11-22 20:42:27 +0300

    Delete IMagican.cs

--- RPGgame-master/RPGgame/IMagican.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RPGgame
-{
-    public interface IMagican
-    {
-        void DoMAgicThing(int Damage, CharacterInfo person);
-        void DoMAgicThing(CharacterInfo person) { }
-        void DoMAgicThing();
-    }
-}
